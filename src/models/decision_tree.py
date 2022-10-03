fdfaee5b45b6ffdc49b6825060d961ab3369c16d
Akshay Kale <akshaykale915@gmail.com>
2022-10-02 23:12:18 -0500

    update: made changes to the decision tree model

--- src/models/decision_tree.py
+++ src/models/decision_tree.py
@@ -25,8 +25,11 @@ def read_csv(csv_file):
 def normalize(_df, columns):
     """
     Function for normalizing the data
+
+    Args:
+        _df (dataframe)
+        columns (features)
     """
-    print("Accessed the feature!")
     for feature in columns:
         _df[feature] = _df[feature].astype(int)
         max_value = _df[feature].max()
@@ -68,6 +71,196 @@ def remove_duplicates(_df, column_name='structureNumbers'):
         temp.append(grouped_df)
     new_df = pd.concat(temp)
     return new_df
+
+def tree_utility(train_x, trainy,
+                 test_x, testy, cols,
+                 criteria='gini', max_depth=7):
+    """
+    Description:
+        Performs the modeling and returns performance metrics
+
+    Args:
+        trainX: Features of Training Set
+        trainy: Ground truth of Training Set
+        testX: Features of Testing Set
+        testy: Ground truth of Testing Set
+
+    Return:
+        acc: Accuracy
+        cm: Confusion Report
+        cr: Classification Report
+        kappa: Kappa Value
+        model: Decision Tree Model
+    """
+    model = DecisionTreeClassifier(criterion=criteria, max_depth=max_depth)
+    #model = HistGradientBoostingClassifier(categorical_features=[], max_depth=maxDepth)
+    model.fit(train_x, trainy)
+    prediction = model.predict(test_x)
+    acc = accuracy_score(testy, prediction)
+    _cm = confusion_matrix(testy, prediction)
+    _cr = classification_report(testy, prediction, zero_division=0)
+    _fi = dict(zip(cols, model.feature_importances_))
+    #rocAuc = roc_auc_score(testy, prediction, multi_class='ovr')
+    kappa = cohen_kappa_score(prediction, testy,
+                              weights='quadratic')
+    return acc, _cm, _cr, kappa, model, _fi # rocAuc, model
+
+
+# Decision Tree
+def decision_tree(X, y, features, label, all_data, nFold=5):
+    """
+    #TODO: We can do things together.
+    def decision_tree(X, y, features, label, nFold=5):
+    Description:
+        Performs training-testing split
+        Train model for various depth level
+        Train model for both Entropy and GiniIndex
+
+    Args:
+        df (Dataframe)
+    """
+    # Kfold cross validation
+    kfold = KFold(nFold, shuffle=True, random_state=1)
+
+    # For storing Confusion Matrix
+    confusionMatrixsEntropy = []
+    confusionMatrixsGini = []
+
+    # For storing Classification Report
+    classReportsEntropy = []
+    classReportsGini = []
+
+    # Scores
+    scoresGini = []
+    scoresEntropy = []
+
+    # ROC AUC 
+    eRocs = []
+    gRocs = []
+
+    # Kappa values
+    gKappaValues = []
+    eKappaValues = []
+
+    # Converting them to array
+    cols = X.columns
+    X = np.array(X)
+    y = np.array(y)
+
+    # Converting all data into array
+    #all_sub_data =  all_data[cols]
+    #structure_numbers = all_data['structureNumber']
+    #all_sub_data = np.array(X)
+    #structure_numbers = np.array(structure_numbers)
+
+    # Store models:
+    eModels = []
+    gModels = []
+
+    # Feature importance
+    eFeatures = []
+    gFeatures = []
+
+    for depth in tqdm(range(1, 31), desc='\n Modeling DT'):
+        tempG = []
+        tempE = []
+        for foldTrainX, foldTestX in kfold.split(X):
+            trainX, trainy, testX, testy = X[foldTrainX], y[foldTrainX], \
+                                          X[foldTestX], y[foldTestX]
+
+            # structure numbers
+            # Gini
+            gacc, gcm, gcr, gkappa, gmodel, gfi = tree_utility(trainX, trainy,
+                                                 testX, testy, cols,
+                                                 criteria='gini',
+                                                 max_depth=depth
+                                                 )
+
+            # Entropy
+            eacc, ecm, ecr, ekappa, emodel, efi = tree_utility(trainX, trainy,
+                                                  testX, testy, cols,
+                                                  criteria='entropy',
+                                                  max_depth=depth )
+            tempG.append(gacc)
+            tempE.append(eacc)
+
+        # Accuracies
+        scoresGini.append(np.mean(tempG))
+        scoresEntropy.append(np.mean(tempE))
+
+        # Confusion Matrix
+        confusionMatrixsEntropy.append(ecm)
+        confusionMatrixsGini.append(gcm)
+
+        # Classification Report
+        classReportsEntropy.append(ecr)
+        classReportsGini.append(gcr)
+
+        # Kappa Values (TODO: select average of Kappa Value)
+        eKappaValues.append(ekappa)
+        gKappaValues.append(gkappa)
+
+        # ROC AUC values(TODO: select average of Kappa Value)
+        #eRocs.append(eroc)
+        #gRocs.append(groc)
+
+        # Models
+        eModels.append(emodel)
+        gModels.append(gmodel)
+
+        # Feature importance
+        eFeatures.append(efi)
+        gFeatures.append(gfi)
+
+    # Performance Summarizer
+    depths = list(range(1, 31))
+
+    # Create Dictionaries
+    # Kappa
+    eKappaDict = dict(zip(eKappaValues, depths))
+    gKappaDict = dict(zip(gKappaValues, depths))
+
+    # Confusion Matrix 
+    eConfDict = dict(zip(depths, confusionMatrixsEntropy))
+    gConfDict = dict(zip(depths, confusionMatrixsGini))
+
+    # Classification Report
+    eClassDict = dict(zip(depths, classReportsEntropy))
+    gClassDict = dict(zip(depths, classReportsGini))
+
+    # Scores (Accuracy)
+    eScoreDict = dict(zip(scoresEntropy, depths))
+    gScoreDict = dict(zip(scoresGini, depths))
+
+    #TODO:  Scores (ROCs) doesn't work
+    #eRocsDict = dict(zip(eRocs, depths))
+    #gRocsDict = dict(zip(gRocs, depths))
+
+    # Models
+    eModelsDict = dict(zip(depths, eModels))
+    gModelsDict = dict(zip(depths, gModels))
+
+    # Feature Importance
+    eFeatureDict = dict(zip(depths, eFeatures))
+    gFeatureDict = dict(zip(depths, gFeatures))
+
+    # Swap X_test with all_data
+    kappaVals, accVals, featImps, models = performance_summarizer(eKappaDict, gKappaDict,
+                                           eConfDict, gConfDict,
+                                           eClassDict, gClassDict,
+                                           eScoreDict, gScoreDict,
+                                           #eRocsDict, gRocsDict,
+                                           eModelsDict, gModelsDict,
+                                           eFeatureDict, gFeatureDict,
+                                           testX, cols, label, all_data)
+
+    # Return the average kappa value for state
+    eBestModel, gBestModel = models
+    #leaves = find_leaves(eBestModel)
+    #splitNodes = print_split_nodes(leaves, eBestModel, features)
+
+    return kappaVals, accVals, featImps, models
+
 
 def main():
     csv_file = '../../data/nebraska_deep.csv'
@@ -165,22 +358,11 @@ def main():
     data_scaled = data_scaled[columns_final]
     data_scaled = remove_null_values(data_scaled)
 
-    # Modeling
-    kmeans_kwargs = {
-                        "init": "random",
-                        "n_init": 10,
-                        "max_iter": 300,
-                        "random_state": 42,
-                    }
-
-    list_of_parameters = ['supNumberIntervention',
-                          'subNumberIntervention',
-                          'deckNumberIntervention'
-                      ]
+    X, y = data_scaled[columns_final], data_scaled[label]
+    decision_tree(X, y, features, label, all_data, nFold=5)
 
     #dataScaled, lowestCount, centroids, counts = kmeans_clustering(data_scaled,
     #                                                               list_of_parameters,
-    #                                                               kmeans_kwargs,
     #                                                               state=state)
 
 main()
